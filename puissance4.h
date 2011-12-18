e7a5fe61bf9b7738289f6ad3690d3addf799b1a5
Damien POMMIES <lapomme-thedims@sfr.fr>
2011-12-18 11:09:06 +0100

    Correction des erreurs des fonctions

--- puissance4.h
+++ puissance4.h
@@ -5,12 +5,13 @@
 #include "afficheGrille.c"
 #include "choix_j.c"
 #include "initialisationgrille.c"
+#include "choix_j2.c"
 
 void menu ();
-void afficheGrille ();
-void choix_j (char j[15]);
-void initialisationGrille();
-
+void afficheGrille (char** grille);
+int choix_j1 (char j[15], char** grille,int hc0,int hc1,int hc2, int hc3, int hc4, int hc5, int hc6);
+void initialisationGrille(char** grille);
+int choix_j2 (char j[15],char** grille,int hc0,int hc1,int hc2, int hc3, int hc4, int hc5, int hc6);
 
 
 
