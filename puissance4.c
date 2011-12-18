8808efcba8b0932b127d65220cbca48c9c26ed7d
Damien POMMIES <lapomme-thedims@sfr.fr>
2011-12-18 11:03:51 +0100

    Ajout incrémentation colonne et boucle do

--- puissance4.c
+++ puissance4.c
@@ -12,11 +12,14 @@ int main () {
   char j2[15];
   int i,j;
   char** grille=NULL;
+  int hc0,hc1,hc2,hc3,hc4,hc5,hc6;
+  int choix_col,choix_col2;
+  int gagne=0;
+  hc0=hc1=hc2=hc3=hc4=hc5=hc6=5;  
   grille=malloc(ROW * sizeof(int));
   for (i=0; i<COL; i++) {
    grille[i]=malloc(COL * sizeof(int));
   }
-
 
   menu();
   
@@ -27,14 +30,64 @@ int main () {
   
   printf("Que la partie commence ! Que le meilleur gagne !\n\n\n");
 
-  initialisationGrille();
-
-  afficheGrille();
-  
-  choix_j(j1);
-
-  afficheGrille();
-
+  initialisationGrille(grille);
+  afficheGrille(grille);
+  do {
+  choix_col=choix_j1(j1,grille,hc0,hc1,hc2,hc3,hc4,hc5,hc6);
+  afficheGrille(grille);
+  switch(choix_col) {
+  case 0:
+    hc0=hc0-1;
+    break;
+  case 1:
+    hc1=hc1-1;
+    break;
+  case 2:
+    hc2=hc2-1;
+    break;
+  case 3:
+    hc3=hc3-1;
+    break;
+  case 4:
+    hc4=hc4-1;
+    break;
+  case 5:
+    hc5=hc5-1;
+    break;
+  case 6:
+    hc6=hc6-1;
+    break;
+  default :
+    break;
+  }
+  choix_col2=choix_j2(j2,grille,hc0,hc1,hc2,hc3,hc4,hc5,hc6);
+  afficheGrille(grille);
+  switch(choix_col) {
+  case 0:
+    hc0=hc0-1;
+    break;
+  case 1:
+    hc1=hc1-1;
+    break;
+  case 2:
+    hc2=hc2-1;
+    break;
+  case 3:
+    hc3=hc3-1;
+    break;
+  case 4:
+    hc4=hc4-1;
+    break;
+  case 5:
+    hc5=hc5-1;
+    break;
+  case 6:
+    hc6=hc6-1;
+    break;
+  default :
+    break;
+  }
+  }while ((gagne !=1) || (gagne != 2));
   free(grille);
     return 0;
 }
